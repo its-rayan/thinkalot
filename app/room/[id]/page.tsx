a1e08a412cae9435700d966bc6a5bf3929499310
its-rayan <rayanmabdirahman@gmail.com>
2026-03-21 17:04:42 +0000

    add question timer

---
+++ app/room/[id]/page.tsx
@@ -0,0 +1,9 @@
+import QuestionTimer from "@/features/room/ui/question-timer";
+
+export default function RoomPage() {
+  return (
+    <div className="flex flex-col w-full gap-4">
+      <QuestionTimer />
+    </div>
+  );
+}
