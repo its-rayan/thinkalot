cbb9d7b8a7cfaa9992ecac30d464b00e32307ae7
its-rayan <rayanmabdirahman@gmail.com>
2026-03-21 16:36:17 +0000

    move layout styles that need overriding from root to home folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,9 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar />
-        <div className="min-h-[calc(100vh-75px)] bg-muted/80 overflow-hidden">
-          <div className="flex flex-col max-w-6xl mx-auto pt-6">{children}</div>
-        </div>
+        {children}
       </body>
     </html>
   );
