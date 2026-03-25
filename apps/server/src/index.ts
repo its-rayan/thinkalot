38a76644d2b6892487a9eaf604360fbb767d5cf4
its-rayan <rayanmabdirahman@gmail.com>
2026-03-25 20:58:36 +0000

    config server app for prod

---
+++ apps/server/src/index.ts
@@ -0,0 +1,11 @@
+import app from "./app";
+
+const PORT = process.env.PORT || 8000;
+
+app
+  .listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  })
+  .on("error", (err) => {
+    console.error("Failed to start server:", err);
+  });
