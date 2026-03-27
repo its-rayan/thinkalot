cb212189605227273cb8b47dd7ad123eb3b65af3
its-rayan <rayanmabdirahman@gmail.com>
2026-03-27 22:06:18 +0000

    update package-lock to reflect name change

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hello, Playtime Server!");
+  res.send("Hello, Thinkalot Server!");
 });
 
 export default app;
